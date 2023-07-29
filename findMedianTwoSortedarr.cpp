1594b88c8f097cb392d5a76b3d5e53950c82343b
Abhisinghs <abhisheksingh35716@gmail.com>
2023-07-30 00:09:56 +0530

    add Leetcode question of medain of two sorted array

---
+++ findMedianTwoSortedarr.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//function that gives us the median of two sorted array
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int i=0;
+        int j=0;
+        int lastindex=-1;
+             
+        // Initialize a new array
+           vector<int>v(n1+n2,0);
+        
+        while(i<n1&&j<n2)
+        {
+            if(nums1[i]<=nums2[j])
+                v[++lastindex]=nums1[i++];
+            else
+                v[++lastindex]=nums2[j++];
+        }
+        
+        while(i<n1)
+            v[++lastindex]=nums1[i++];
+        while(j<n2)
+            v[++lastindex]=nums2[j++];
+        
+    // Return the result
+        int n=n1+n2;
+        return n%2?v[n/2]:(v[n/2]+v[n/2-1])/2.0;
+        
+    }
+
+    int main(){
+
+        vector<int> arr({1,3});
+        vector<int>arr2({2});
+
+
+        // call the function
+        int median=findMedianSortedArrays(arr,arr2);
+        cout<<"the median of sorted array is "<<median;
+        return 1;
+    }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+arr.reduce((el,index)={
+
+// })
